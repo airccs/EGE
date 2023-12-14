ea13907dad27150530e354e7aedd11886a4f4a4c
airccs <iljakir.06@gmail.com>
2023-12-14 21:26:10 +0300

    new

---
+++ №16/16.py
@@ -0,0 +1,13 @@
+"""def F(n):
+    if n < 2:
+        return 1
+    if n >= 2:
+        return F(n - 1) * (n + 5)
+print(F(9))"""
+
+"""def F(n):
+    if n==1:
+        return 3
+    if n > 1:
+        return 4 * F(n - 1) - 3 * n
+print(F(5))"""
