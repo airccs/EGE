3a4aee245e3d78eb14805c7beeef70f197c39a33
airccs <iljakir.06@gmail.com>
2023-12-14 21:27:24 +0300

    new

--- №16/16.py
+++ №16/16.py
@@ -5,9 +5,9 @@
         return F(n - 1) * (n + 5)
 print(F(9))"""
 
-"""def F(n):
+def F(n):
     if n==1:
         return 3
     if n > 1:
         return 4 * F(n - 1) - 3 * n
-print(F(5))"""
+print(F(5))
