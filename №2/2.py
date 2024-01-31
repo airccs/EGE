94163fa0d5f87c1f913790eb126c3d7a19751cd0
airccs <iljakir.06@gmail.com>
2024-01-31 18:52:04 +0300

    new

--- №2/2.py
+++ №2/2.py
@@ -3,6 +3,6 @@ for x in range(2):
     for y in range(2):
         for w in range(2):
             for z in range(2):
-                F = (x and not y)or(y==z)or not w
+                F = (x and not y)or (x==z) or not w
                 if F == 0:
                     print(x, y, w, z, int(F))
