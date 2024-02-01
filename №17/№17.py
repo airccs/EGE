a7a78249d85bd1deaa002e3887f8b891c930783e
airccs <iljakir.06@gmail.com>
2024-02-01 23:35:48 +0300

    new

--- №17/№17.py
+++ №17/№17.py
@@ -16,4 +16,14 @@ sum = []
 for i in range(len(a) - 1):
     if a[i] > 850 or a[i + 1] > 850:
         sum.append(a[i] ** 2 + a[i + 1] ** 2)
-print(len(sum), max(sum))'''
+print(len(sum), max(sum))
+
+f = open('№17/17')
+a = []
+for s in f:
+    a.append(int(s))
+sum = []
+for i in range(len(a)-2):
+    if (a[i] > 0 or a[i + 1] > 0 or a[i + 2] > 0):
+        sum.append(a[i] + a[i + 1] + a[i + 2])
+print(len(sum),min(sum))'''
