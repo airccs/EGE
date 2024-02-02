d5debec6a1efa5b997ba1bc9a2b133b3fe78f790
airccs <iljakir.06@gmail.com>
2024-02-03 00:13:48 +0300

    new

--- №17/№17.py
+++ №17/№17.py
@@ -18,12 +18,13 @@ for i in range(len(a) - 1):
         sum.append(a[i] ** 2 + a[i + 1] ** 2)
 print(len(sum), max(sum))
 
-f = open('№17/17')
-a = []
-for s in f:
-    a.append(int(s))
+f = open('17.txt')
+a = [int(s) for s in f]
+chet = [x for x in a if x % 2 == 0]
+sr_ar = sum(chet) / len(chet)
 sum = []
-for i in range(len(a)-2):
-    if (a[i] > 0 or a[i + 1] > 0 or a[i + 2] > 0):
-        sum.append(a[i] + a[i + 1] + a[i + 2])
-print(len(sum),min(sum))'''
+for i in range(len(a) - 1):
+    if ((a[i] % 3 == 0) or (a[i + 1] % 3 == 0)) and ((a[i] < sr_ar) or (a[i + 1] < sr_ar)):
+        sum.append(a[i] + a[i + 1])
+print(len(sum),max(sum))
+'''
