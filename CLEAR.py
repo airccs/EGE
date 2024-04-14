f6d5e120ab891981ac7161f66ba0f23a4e63ac93
airccs <iljakir.06@gmail.com>
2024-04-14 19:40:49 +0300

    new

--- CLEAR.py
+++ CLEAR.py
@@ -1,1 +1,13 @@
-123131
+from turtle import *
+k = 20
+tracer(0)
+pendown()
+for i in range(4):
+    circle(4*k,180)
+    right(90)
+penup()
+for x in range(-20,20):
+    for y in range(-100, 100):
+        setpos(x*k,y*k)
+        dot()
+done()
